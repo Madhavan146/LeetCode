429eabcb0c0088a0b9ccb704edfb3e662f49bbf8
Madhavan146 <hariharan22446688@gmail.com>
2025-04-13 16:20:59 +0530

    Time: 10 ms (5.58%), Space: 8.2 MB (89.77%) - LeetHub

--- 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.c
+++ 2116-count-number-of-pairs-with-absolute-difference-k/2116-count-number-of-pairs-with-absolute-difference-k.c
@@ -1,18 +1,13 @@
 int countKDifference(int* a, int n, int k) {
-
 int i,j,c,t;
 c=0;
     for(i=0;i<n;i++)
-    {
         for(j=0;j<n;j++)
         {
-            if(a[i]-a[j]==k ||  a[j]-a[i]==k )
+            if(a[i]-a[j]==k||a[j]-a[i]==k )
                c=c+1;
         }
-    }
     if(c>0)
        t=c/2;
-
-
        return t;
 }
