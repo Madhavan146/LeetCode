644109be52767f83f6ca1966ba675707920f5083
Madhavan146 <hariharan22446688@gmail.com>
2025-07-10 14:00:43 +0530

    Time: 228 ms (6.19%), Space: 8.8 MB (17.7%) - LeetHub

--- 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.c
+++ 1221-element-appearing-more-than-25-in-sorted-array/1221-element-appearing-more-than-25-in-sorted-array.c
@@ -1,33 +1,15 @@
 int findSpecialInteger(int* a, int n) {
-
-
-
-int i,j,k,r,c,t,f;
-c=0;t=0;
-r=0;
+int i,j,r=0,c=0,t=0;
   for(i=0;i<n;i++)
-  {
-   
-        c=0;
-       
-        for(j=i;j<n;j++)
-        {
+  {   
+        c=0;     
+        for(j=i;j<n;j++)       
             if(a[i]==a[j])
-            {
-                c+=1;
-            }
-            else
-            {
-                break;
-            }
-         
-      if(c>r)
-        {
+                c+=1;          
+            else{
+            break;}     
+      if(c>r){
             r=c;
-            t=a[i];
-        }
-        }
-        }
-        return t;
-}
+            t=a[i];}}
+        return t;}
  
