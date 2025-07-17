3aedf30c866713b3b677ea9e7a198d2f53592ea4
Madhavan146 <hariharan22446688@gmail.com>
2025-07-17 19:56:48 +0530

    Time: 2 ms (71.15%), Space: 57.4 MB (48.65%) - LeetHub

---
+++ 3778-transform-array-by-parity/3778-transform-array-by-parity.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} a
+ * @return {number[]}
+ */
+var transformArray = function(a) {
+
+n=a.length;
+b=[];
+e=0;
+o=0;
+for(i=0;i<n;i++)
+{
+    if(a[i]%2==0)
+    {
+        e+=1;
+    }
+    else
+    {
+        o+=1;
+    }
+}
+for(i=0;i<e;i++)
+{
+    b.push(0);
+}
+for(i=e;i<n;i++)
+{
+    b.push(1);
+}
+       return b ;
+    
+};
