e143a7649d569b160a61393b87eee73b094afeaa
Madhavan146 <hariharan22446688@gmail.com>
2025-07-19 14:25:12 +0530

    Time: 4 ms (7.23%), Space: 58.8 MB (33.67%) - LeetHub

--- 2058-concatenation-of-array/2058-concatenation-of-array.js
+++ 2058-concatenation-of-array/2058-concatenation-of-array.js
@@ -3,18 +3,13 @@
  * @return {number[]}
  */
 var getConcatenation = function(s) {
-
-//let k,b,c,i;
-
 k=[];
-b=[];
 c=[];
     for(i=0;i<s.length;i++)
     {
-      b.push(s[i]);
       c.push(s[i]);
     }
-       k= b.concat(c); 
+       k= s.concat(c); 
         return k;
     
 };
